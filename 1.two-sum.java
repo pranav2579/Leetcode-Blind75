b322d24ae42c28bc0f02a99c8add856f1291b66a
Pranav Tripathi <pranav2579@gmail.com>
2023-12-18 13:23:28 -0800

    1. Two Sum

---
+++ 1.two-sum.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int solution[]  = new int[2];
+
+        Map<Integer,Integer> numsMap = new HashMap<Integer,Integer>();
+        for(int i = 0; i< nums.length; i++)
+        {
+            if(numsMap.containsKey(target-nums[i])){
+                return (new int[] {numsMap.get(target-nums[i]),i});
+            }
+            numsMap.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+
+    }
+}
+// @lc code=end
+
