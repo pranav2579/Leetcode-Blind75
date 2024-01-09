dca4c95624384d24664ef7857d773cc5ab60a5d0
Pranav Tripathi <pranav2579@gmail.com>
2024-01-08 21:30:50 -0800

    647. Palindromic Substrings

---
+++ 647.palindromic-substrings.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=647 lang=java
+ *
+ * [647] Palindromic Substrings
+ */
+
+// @lc code=start
+class Solution {
+    public int countSubstrings(String s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); ++i) {
+            // odd-length palindromes, single character center
+            ans += countPalindromesAroundCenter(s, i, i);
+
+            // even-length palindromes, consecutive characters center
+            ans += countPalindromesAroundCenter(s, i, i + 1);
+        }
+
+        return ans;
+    }
+
+    private int countPalindromesAroundCenter(String ss, int lo, int hi) {
+        int ans = 0;
+
+        while (lo >= 0 && hi < ss.length()) {
+            if (ss.charAt(lo) != ss.charAt(hi))
+                break;      // the first and last characters don't match!
+
+            // expand around the center
+            lo--;
+            hi++;
+
+            ans++;
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
